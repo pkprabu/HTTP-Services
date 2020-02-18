ddd7ca067a12c962272d98ca53541172b1f1abb4
SEO India <pkprabu07@gmail.com>
2020-02-19 00:31:48 +0530

    heroku deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { AppErrorHandler } from './../../common/app-specific-errors/app-error-handler';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,21 +11,43 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { NotFoundComponent } from './not-found/not-found.component';
 import { CreateProductFormComponent } from './create-product-form/create-product-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+
+const routes: Routes = [
+  {path: '', component: ProductsComponent},
+  {path: 'products', component: ProductsComponent },
+  {path: 'lazy-loading', loadChildren: './lazy/lazy.module#LazyModule' },
+  {path: 'create-product', component: CreateProductFormComponent },
+  {path: 'products/:id/:productName', component: ProductDetailsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '**', component: NotFoundComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     ProductDetailsComponent,
     NotFoundComponent,
-    CreateProductFormComponent
+    CreateProductFormComponent,
+    LoginComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    InfiniteScrollModule
   ],
-  providers: [],
+  providers: [
+    {provide: ErrorHandler, useClass: AppErrorHandler}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
